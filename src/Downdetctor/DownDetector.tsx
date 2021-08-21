47a83b4e1edd52ed025eb16c8cafd3362bc28b7a
Max Boyar <mboyar54@gmail.com>
2021-08-21 03:21:56 +0300

    Chaging backend

--- src/Downdetctor/DownDetector.tsx
+++ src/Downdetctor/DownDetector.tsx
@@ -63,8 +63,10 @@ function DownDetector() {
   }
 
     const fetchApi=async()=>{
-        const response = await fetch("https://63410.wayscript.io/");
+        //const response = await fetch("https://63410.wayscript.io/");
+        const response = await fetch("http://downdetector-service-api.herokuapp.com/");
         const json:ApiResponse = await response.json();
+        console.log(json)
         const history = await fetchHistory();
     
         setDownStatus([
